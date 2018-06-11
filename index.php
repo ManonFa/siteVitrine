554e13a3083d4d7770b4225b5cbcf605e751c44b
ManonFa <40165304+ManonFa@users.noreply.github.com>
2018-06-11 12:32:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+ <?php 
+ //Creation de la page d accueil
+
+session_start();
+require_once "../includes/functions.php"; 
+require_once "../includes/header.php";
+?>
+
+
+
+
+<?php require_once "../includes/footer.php"; ?>
